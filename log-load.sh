78abe210647c1f5b8b4e7b604581fef8972a3cb2
someone <some@n.e>
2017-10-17 12:42:14 +0100

    log full cpu and memory

--- log-load.sh
+++ log-load.sh
@@ -1,6 +1,9 @@
 while true; do 
-(echo "%CPU %MEM ARGS $(date)" && ps -e -o pcpu,pmem,args --sort=pcpu | cut -d" " -f1-5 | tail) >> cpu.log; sleep 5; 
-(echo "%MEM %CPU ARGS $(date)" && ps -e -o pmem,pcpu,args --sort=pmem | cut -d" " -f1-5 | tail) >> memory.log; sleep 5; 
+(echo "%CPU %MEM ARGS $(date)" && ps -e -o pcpu,pmem,args --sort=pcpu | cut -d" " -f1-5 | tail) >> cpu.log; 
+(echo "Total CPU usage *10: " && ps aux | sed 's/\.//' | tail -n +2 | awk '{sum+=$3} END {print sum}') >> cpu.log;
+(echo "%MEM %CPU ARGS $(date)" && ps -e -o pmem,pcpu,args --sort=pmem | cut -d" " -f1-5 | tail) >> memory.log; 
+(echo && free -m) >> memory.log
+sleep 5; 
 done
 
 
