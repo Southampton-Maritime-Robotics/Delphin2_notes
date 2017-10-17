4e5773d407f05c67d4cd684df0df02290b6ce053
someone <some@n.e>
2017-10-17 13:59:52 +0100

    add total cpu and memory usage

--- log-load.sh
+++ log-load.sh
@@ -3,7 +3,7 @@ while true; do
 (echo "Total CPU usage *10: " && ps aux | sed 's/\.//' | tail -n +2 | awk '{sum+=$3} END {print sum}') >> cpu.log;
 (echo "%MEM %CPU ARGS $(date)" && ps -e -o pmem,pcpu,args --sort=pmem | cut -d" " -f1-5 | tail) >> memory.log; 
 (echo && free -m) >> memory.log
-sleep 5; 
+sleep 2; 
 done
 
 
